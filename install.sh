026181ad34843f4ae2c558ce2e00a51982a2591b
Michael Borglin <Borglin.Me@gmail.com>
2015-06-12 21:48:04 -0700

    Added install for Windows and notification for Linux

--- install.sh
+++ install.sh
@@ -4,9 +4,9 @@ if [ "$(uname)" == "Darwin" ]; then
   echo ". "`pwd`"/z.sh" >> ~/.zshrc
   echo ". "`pwd`"/z.sh" >> ~/.bashrc
 elif [ "$(expr substr $(uname -s) 1 5)" == "Linux" ]; then
-  # Do something under Linux platform
-  echo "linux"
+  echo "Put something like this in your $HOME/.bashrc or $HOME/.zshrc: '. /path/to/z.sh'"
 elif [ "$(expr substr $(uname -s) 1 10)" == "MINGW32_NT" ]; then
-  # Do something under Windows NT platform
-  echo "windows"
+  echo ". "`pwd`"/z.sh" >> ~/.bash_profile
+  echo ". "`pwd`"/z.sh" >> ~/.zshrc
+  echo ". "`pwd`"/z.sh" >> ~/.bashrc
 fi
